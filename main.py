002d6c1f1a61611b7c429f07f351ec873a5a44dc
Мыльцев Александр Михайлович <kotobars@gmail.com>
2024-05-13 22:30:17 +0300

    Realease 1: user context, system message

--- main.py
+++ main.py
@@ -14,27 +14,32 @@ groq_client = Groq(api_key=os.getenv('GROQ_API_KEY'))
 bot = Bot(token=os.getenv('TELEGRAM_BOT_TOKEN'))
 dp = Dispatcher()
 
-contex = []
+user_contexts = {}  # Dictionary to store context for each user
 
 
 @dp.message(F.text)
 async def welcome(message: types.Message):
     await bot.send_chat_action(message.chat.id, 'typing')
 
-    global contex
+    user_id = str(message.from_user.id)
+    if user_id not in user_contexts:
+        user_contexts[user_id] = [{"role": "system", "content": "Ты ассистент, которого зовут StudentLLMAbot, "
+                                                                "ты помогаешь студентам с учёбой. Отвечай всегда "
+                                                                "по-русски, только если тебя не попросят помочь с "
+                                                                "английским языком"}]
 
-    contex.append({"role": 'user', "content": message.text})
+    user_context = user_contexts[user_id]
+    user_context.append({"role": 'user', "content": message.text})
 
-    if len(contex) > 10:
-        messages = contex[-10:]
+    if len(user_context) > 10:
+        user_context = user_context[-10:]
+
     response = groq_client.chat.completions.create(model='llama3-70b-8192',
-                                                   messages=contex, temperature=0)
+                                                   messages=user_context, temperature=0, user=user_id)
 
-    contex.append({"role": 'assistant', "content": response.choices[0].message.content})
+    user_context.append({"role": 'assistant', "content": response.choices[0].message.content})
 
     text = response.choices[0].message.content
-
-    # print(contex)
 
     await message.answer(text)
 
